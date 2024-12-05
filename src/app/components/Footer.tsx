cb7c4ceac39e98902e66d9fc32d03bcce121998a
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-12-05 19:35:33 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer(){
             <FontAwesomeIcon icon={faInstagram} className="h-7 duration-200 hover:scale-125 cursor-pointer"/>
             </div>
             <div className="flex justify-center items-center gap-2">
-                <p className="text-sm md:text-lg font-semibold text-gray-400">Powered by <span className="underline text-white cursor-pointer hover:text-gray-200">Indolj</span></p>
+                <p className="text-sm md:text-lg font-semibold text-gray-400">Powered by <span className="underline text-white cursor-pointer hover:text-gray-200">UMM-E-HANI</span></p>
                 <span>|</span>
                 <p className="text-sm md:text-lg underline cursor-pointer hover:text-gray-200">Privacy Policy</p>
                 <span>|</span>
